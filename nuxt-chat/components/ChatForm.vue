511104ada8e9aa08d225b4d2dc1dcd6c35931de9
Anton Volkov <volkovanton71@yandex.ru>
2020-10-27 17:34:43 +0300

    created form & message

---
+++ nuxt-chat/components/ChatForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-flex sc12>
+    <v-text-field
+      label="Введите сообщение"
+      outline
+      v-model="text"
+      @keydown.enter="send"
+    />
+  </v-flex>
+</template>
+<script>
+export default {
+  data: () => ({
+    text: "",
+  }),
+  methods: {
+    send() {
+      this.$socket.emit(
+        "createMessage",
+        {
+          text: this.text,
+          id: this.$store.state.user.id,
+        },
+        (data) => {
+          if (typeof data === "string") {
+            console.log(data);
+          } else {
+             this.text = "";
+          }
+        }
+      );
+    },
+  },
+};
+</script>
---
+++ nuxt-chat/server/app.js
@@ -0,0 +1,38 @@
+const app = require('express')()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+const m = (name, text, id) => ({ name, text, id })
+io.on('connection', socket => {
+
+  socket.on("userJoined", (data, cb) => {
+    if (!data.name || !data.room) {
+      return cb("Данные некорректны")
+    }
+
+    socket.join(data.room)
+
+    cb({ userId: socket.id })
+    socket.emit("newMessage", m("admin", `Добро пожаловать ${data.name}.`))
+    socket.emit("newMessage", m("TEST", `Добро пожаловать.`))
+    socket.broadcast.to(data.room)
+    .emit("newMessage", m("admin", `Пользователь ${data.name} зашел.`))
+  })
+
+
+  socket.on("createMessage", data => {
+    setTimeout(() => {
+      socket.emit("newMessage", {
+        text: data.text + 'SERVER'
+      })
+    }, 1000)
+
+  })
+
+
+})
+
+module.exports = {
+  app,
+  server
+}
